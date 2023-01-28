97abc56dfd3df4f9b22a32b51166cef9be63f174
Progna_Labony_Roy <labony.progna@gmail.com>
2023-01-28 08:35:34 +0600

    4.prime or not solved

---
+++ prime.js
@@ -0,0 +1,18 @@
+// Q.4. Write a program in JS to check whether a number is prime or not?
+
+// user input
+function isPrime(n) {
+    if(n<2){
+        return `${n} is not a prime number`
+    }
+
+    for(let i=2; i<n; i++){
+        if(n%i === 0){
+            return `${n} is not a prime number`
+        }
+    }
+    return `${n} is a prime number`
+}
+
+
+console.log(isPrime(11));
